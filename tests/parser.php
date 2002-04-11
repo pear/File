967840e437d4766b60467c6709a1e281e4781d59
Tomas V.V.Cox <cox@php.net>
2002-04-11 02:03:40 +0000

    Enhance test to the new features
    git-svn-id: http://svn.php.net/repository/pear/packages/File/trunk@77629 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/parser.php
+++ tests/parser.php
@@ -2,21 +2,30 @@
 $path = ini_get('include_path');
 ini_set('include_path', realpath('../') . ":$path");
 require_once 'File/CSV.php';
+
+/*/Example conf:
 $conf = array(
     'fields' => 4,
     'sep'    => "\t",
     'quote'  => '"',
-    'header' => true
+    'header' => false
 );
+//*/
 ob_implicit_flush(true);
 $argv = $_SERVER['argv'];
 $file = $argv[1];
+$write = (isset($argv[2])) ? $argv[2] : false;
 PEAR::setErrorHandling(PEAR_ERROR_PRINT, "warning: %s\n");
-$i = 0;
+
+$conf = File_CSV::discoverFormat($file);
 while ($fields = File_CSV::read($file, $conf)) {
+    if ($write) {
+        File_CSV::write($write, $fields, $conf);
+    }
     print_r($fields);
-    //if ($i++ >= 2) break;
 }
+
+var_dump($conf);
 echo "\n"
 
 ?>
