edb9fd904234895655237de109d406277522dd91
Michael Wallner <mike@php.net>
2004-12-17 11:04:25 +0000

    * updated header comment block
    git-svn-id: http://svn.php.net/repository/pear/packages/File/trunk@174956 c90b9560-bf6c-de11-be94-00142212c4b1

--- File.php
+++ File.php
@@ -1,20 +1,15 @@
 <?php
-//* vim: set expandtab tabstop=4 shiftwidth=4: */
 // +----------------------------------------------------------------------+
-// | PHP Version 4                                                        |
+// | PEAR :: File                                                         |
 // +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2003 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
+// | This source file is subject to version 3.0 of the PHP license,       |
+// | that is available at http://www.php.net/license/3_0.txt              |
+// | If you did not receive a copy of the PHP license and are unable      |
+// | to obtain it through the world-wide-web, please send a note to       |
 // | license@php.net so we can mail you a copy immediately.               |
 // +----------------------------------------------------------------------+
-// | Authors: Richard Heyes <richard@php.net>                             |
-// |          Tal Peer <tal@php.net>                                      |
+// | Authors and Copyright: Richard Heyes <richard@php.net>               |
+// |                        Tal Peer <tal@php.net>                        |
 // +----------------------------------------------------------------------+
 // 
 // $Id$
@@ -24,6 +19,7 @@
  * 
  * @package     File
  * @category    FileSystem
+ * @license     PHP License 3.0
  * @version     $Revision$
  */
 
@@ -193,6 +189,7 @@ class File extends PEAR
             }
             return $file;
         }
+        
         $file = '';
         while (false !== $buf = File::read($filename, FILE_DEFAULT_READSIZE, $lock)) {
             if (PEAR::isError($buf)) {
