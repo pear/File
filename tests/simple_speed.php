0335246bef85249fd10c51228fbf676ecadaf892
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2008-10-26 22:16:07 +0000

    update the simple speed test
    git-svn-id: http://svn.php.net/repository/pear/packages/File/trunk@267833 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/simple_speed.php
+++ tests/simple_speed.php
@@ -1,17 +1,19 @@
 <?php
+error_reporting(1803);
 /* There's nothing scientific about this but it does
  * catch it when we messing up badly speed wise, talking about
  * couple of second speed decrease
  */
 require_once 'File/CSV.php';
 $file = '100000-rows__2-columns.csv';
-$conf = File_CSV::discoverFormat($file);
+//$conf = File_CSV::discoverFormat($file);
+$conf = array('fields' => 2, 'sep' => ',', 'quote' => '"');
 
-$_start_time = time();
 
-while ($row = File_CSV::read($file, $conf)) {
-    // some random process
-    $foo = substr($row[0] ,0 , 1);
-}
+//Run the test with quotes and without them, NULL that is.
 
-echo "\n" . 'Processing time: ', time()-$_start_time, ' seconds.';
+echo "\nRemember to turn off XDebug";
+$_start_time = microtime(true);
+while ($row = File_CSV::read($file, $conf)) {}
+
+echo "\n" . 'Processing time: ', (microtime(true)-$_start_time), ' seconds.' . "\n";
